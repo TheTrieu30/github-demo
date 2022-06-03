cd9da6b80a7c0665029fbe1a60a6b27d00b23b4d
TheTrieu30 <106759778+TheTrieu30@users.noreply.github.com>
2022-06-03 23:05:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function sum(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    let sum = parseInt(a) + b;
+
+    document.getElementById('result').innerHTML = sum;
